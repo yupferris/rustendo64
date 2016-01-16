2ef6d481a5ffcd1cf0e2089e957fd249101eca99
ferris <jake@fusetools.com>
2016-01-15 20:10:37 -0800

    Started loading ROM's

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+use std::io::Read;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let pif_file_name = env::args().nth(1).unwrap();
+    let rom_file_name = env::args().nth(2).unwrap();
+
+    let pif = read_bin(pif_file_name);
+    let rom = read_bin(rom_file_name);
+}
+
+fn read_bin<P: AsRef<Path>>(path: P) -> Vec<u8> {
+    let mut file = fs::File::open(path).unwrap();
+    let mut file_buf = Vec::new();
+    file.read_to_end(&mut file_buf).unwrap();
+    file_buf
 }
