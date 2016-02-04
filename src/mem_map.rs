8eec6e3c90f63d3ca960edf4d339a5d2159a2e87
ferris <jake@fusetools.com>
2016-02-04 09:53:20 +0100

    And mem_map.rs :)
    Forgot this one too. Geez, I'm so out of it these days!

---
+++ src/mem_map.rs
@@ -0,0 +1,6 @@
+pub const PIF_ROM_START: u32 = 0x1fc0_0000;
+pub const PIF_ROM_LENGTH: u32 = 2048;
+pub const PIF_ROM_END: u32 = PIF_ROM_START + PIF_ROM_LENGTH;
+
+pub const SP_BASE_REG: u32 = 0x0404_0000;
+pub const SP_STATUS_REG: u32 = 0x0404_0010;
