9f84f2980d0986a5968aa9b9f19699dc1db2ea70
Thomas Hansen <dryvnt@gmail.com>
2016-02-23 21:21:20 +0100

    Minor clarity change

--- src/cpu/cp0/reg_config.rs
+++ src/cpu/cp0/reg_config.rs
@@ -7,7 +7,7 @@ pub struct RegConfig {
     endianness: Endianness,
 
     cu: bool,
-    kseg0_cache_bits: [bool;3],
+    kseg0_cache_enable_bits: [bool;3],
     kseg0_cache_enabled: bool
 }
 
@@ -19,7 +19,7 @@ impl From<u32> for RegConfig {
             endianness: value.into(),
 
             cu: (value & (1 << 3)) != 0,
-            kseg0_cache_bits: [
+            kseg0_cache_enable_bits: [
                 (value & (1 << 0)) != 0,
                 (value & (1 << 1)) != 0,
                 (value & (1 << 2)) != 0,
