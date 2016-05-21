619611289eb6214d6ab697a7c02f5ed09f3f0062
Phlosioneer <mattmdrr2@gmail.com>
2016-05-21 16:10:25 -0400

    Removed tabs.

--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,16 @@ fn main() {
     let rom_file_name_opt = env::args().nth(2);
 
     let (pif_file_name, rom_file_name) = 
-	    match (pif_file_name_opt, rom_file_name_opt) {
-	        (Some(pif), Some(rom)) => (pif, rom),
-	        _ => {print_usage(); return}
-	    };
+        match (pif_file_name_opt, rom_file_name_opt) {
+            (Some(pif), Some(rom)) => (pif, rom),
+            _ => {print_usage(); return}
+        };
 
     let (pif, rom) = 
-	    match (read_bin(pif_file_name), read_bin(rom_file_name)) {
-	        (Some(pif), Some(rom)) => (pif, rom),
-	        _ => return,
-	    };
+        match (read_bin(pif_file_name), read_bin(rom_file_name)) {
+            (Some(pif), Some(rom)) => (pif, rom),
+            _ => return,
+        };
     
     let mut n64 = n64::N64::new(pif, rom);
     loop {
@@ -60,26 +60,26 @@ fn read_bin<P: AsRef<Path>>(path: P) -> Option<Box<[u8]>> {
         Ok(v) => v,
         Err(e) => {
             println!("Unable to open file \"{}\" from current directory \"{}\": {}",
-		             path.as_ref().display(), 
-		             Path::new(".").canonicalize().unwrap().display(),
-		             e);
+                     path.as_ref().display(), 
+                     Path::new(".").canonicalize().unwrap().display(),
+                     e);
             return None;
         }
     };
-	
-	// Create a box without flooding the stack.
-	let pif_size = mem_map::PIF_ROM_LENGTH as usize;
-	let mut temp_vec: Vec<u8> = Vec::with_capacity(pif_size);
+    
+    // Create a box without flooding the stack.
+    let pif_size = mem_map::PIF_ROM_LENGTH as usize;
+    let mut temp_vec: Vec<u8> = Vec::with_capacity(pif_size);
     return match file.read_to_end(&mut temp_vec) {
         Ok(_) => Some(temp_vec.into_boxed_slice()),
         Err(e) => {
             match e.kind() {
-	            	ErrorKind::UnexpectedEof => {
-	            		println!("Error reading pif rom: file is too small.");
-	            	},
-	            	e => {
-	            		println!("IO Error while reading pif rom: {:?}", e);
-	            	}
+                ErrorKind::UnexpectedEof => {
+                    println!("Error reading pif rom: file is too small.");
+                },
+                e => {
+                    println!("IO Error while reading pif rom: {:?}", e);
+                }
             };
             None
         }
