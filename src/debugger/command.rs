a1733416b06cb552398bca84ace5653de5850faf
ferris <jake@fusetools.com>
2016-06-04 16:06:53 +0200

    Added optional count parameter for step command

--- src/debugger/command.rs
+++ src/debugger/command.rs
@@ -1,21 +1,22 @@
-use std::str::FromStr;
+use std::borrow::Cow;
+use std::str::{self, FromStr};
 
-use nom::{IResult, eof};
+use nom::{IResult, eof, space, digit};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Command {
-    Step,
+    Step(usize),
     Exit,
     Repeat,
 }
 
 impl FromStr for Command {
-    type Err = String;
+    type Err = Cow<'static, str>;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match command(s.as_bytes()) {
             IResult::Done(_, c) => Ok(c),
-            err => Err(format!("Unable to parse command: {:?}", err))
+            err => Err(format!("Unable to parse command: {:?}", err).into())
         }
     }
 }
@@ -32,9 +33,10 @@ named!(
 
 named!(
     step<Command>,
-    map!(
-        alt_complete!(tag!("step") | tag!("s")),
-        |_| Command::Step));
+    chain!(
+        alt_complete!(tag!("step") | tag!("s")) ~
+            count: opt!(preceded!(space, usize_parser)),
+        || Command::Step(count.unwrap_or(1))));
 
 named!(
     exit<Command>,
@@ -45,3 +47,11 @@ named!(
 named!(
     repeat<Command>,
     value!(Command::Repeat));
+
+named!(
+    usize_parser<usize>,
+    map_res!(
+        map_res!(
+            digit,
+            str::from_utf8),
+        FromStr::from_str));
--- src/debugger/mod.rs
+++ src/debugger/mod.rs
@@ -2,6 +2,7 @@ mod command;
 
 use std::io::{stdin, stdout};
 use std::io::prelude::*;
+use std::borrow::Cow;
 use n64::cpu::Instruction;
 use n64::cpu::opcode::Opcode::*;
 use n64::mem_map;
@@ -31,45 +32,47 @@ impl Debugger {
 
             let command = match (read_stdin().parse(), self.last_command) {
                 (Ok(Command::Repeat), Some(c)) => Ok(c),
-                (Ok(Command::Repeat), None) => Err("No last command"),
+                (Ok(Command::Repeat), None) => Err("No last command".into()),
                 (Ok(c), _) => Ok(c),
-                (Err(_), _) => Err("Invalid input"),
+                (Err(e), _) => Err(e),
             };
 
             match command {
-                Ok(Command::Step) => self.step(),
+                Ok(Command::Step(count)) => self.step(count),
                 Ok(Command::Exit) => break,
                 Ok(Command::Repeat) => unreachable!(),
-                Err(e) => println!("{}", e),
+                Err(ref e) => println!("{}", e),
             }
 
             self.last_command = command.ok();
         }
     }
 
-    pub fn step(&mut self) {
-        let current_pc = self.n64.cpu().current_pc_phys();
-        let addr = mem_map::map_addr(current_pc as u32);
-        let instr = Instruction(match addr {
-            PifRom(offset) => self.n64.interconnect().pif().read_boot_rom(offset),
-            _ => panic!("Debugger can't inspect address: {:?}", addr),
-        });
+    pub fn step(&mut self, count: usize) {
+        for _ in 0..count {
+            let current_pc = self.n64.cpu().current_pc_phys();
+            let addr = mem_map::map_addr(current_pc as u32);
+            let instr = Instruction(match addr {
+                PifRom(offset) => self.n64.interconnect().pif().read_boot_rom(offset),
+                _ => panic!("Debugger can't inspect address: {:?}", addr),
+            });
 
-        print!("{:018X}: ", current_pc);
+            print!("{:018X}: ", current_pc);
 
-        match instr.opcode() {
-            Special => print!("{:?} (Special)", instr.special_op()),
-            RegImm => print!("{:?} (RegImm)", instr.reg_imm_op()),
-            _ => print!("{:?}", instr),
+            match instr.opcode() {
+                Special => print!("{:?} (Special)", instr.special_op()),
+                RegImm => print!("{:?} (RegImm)", instr.reg_imm_op()),
+                _ => print!("{:?}", instr),
+            }
+
+            if self.n64.cpu().will_execute_from_delay_slot() {
+                println!(" (DELAY)");
+            } else {
+                println!("");
+            }
+
+            self.n64.step();
         }
-
-        if self.n64.cpu().will_execute_from_delay_slot() {
-            println!(" (DELAY)");
-        } else {
-            println!("");
-        }
-
-        self.n64.step();
     }
 }
 
